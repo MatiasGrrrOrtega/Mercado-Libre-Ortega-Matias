4f6483a5784aff808a30ae210a8d3b2529a7e1ce
MatiasOrtega <mat-1998-ort@hotmail.com>
2024-01-22 03:51:15 -0300

    proyecto mercado libre

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+
+app.use("/public", express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
