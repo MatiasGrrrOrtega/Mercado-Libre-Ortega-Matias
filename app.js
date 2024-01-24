02f1a84a02a88055d24ea9d584d51692c934c383
MatiasOrtega <mat-1998-ort@hotmail.com>
2024-01-23 22:23:22 -0300

    Update package.json and style.css, remove index.js and modify login.html and register.html

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const app = express()
 const path = require('node:path')
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3001;
 
-app.use('/public', express.static('public'))
+app.use('/public',express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/home.html'))
-});
-
-app.get('/register', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/register.html'))
 });
 
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/login.html'))
 });
 
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/register.html'))
+});
+
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost/${port}`)
+  console.log(`Server is running on http://localhost:${port}`)
 });
