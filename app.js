e2ce4589c1050e63700e709962b4300b1f5d395f
MatiasOrtega <mat-1998-ort@hotmail.com>
2024-01-23 22:47:25 -0300

    Refactor file paths in app.js and views files

--- app.js
+++ app.js
@@ -5,16 +5,19 @@ const port = process.env.PORT || 3001;
 
 app.use('/public',express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/home.html'))
-});
+app.get('/', (req, res) => 
+  // res.sendFile(path.join(__dirname, '/views/home.html'))
+  res.sendFile(`${__dirname}/views/home.html`)
+);
 
 app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/login.html'))
+  // res.sendFile(path.join(__dirname, '/views/login.html'))
+  res.sendFile(`${__dirname}/views/login.html`)
 });
 
 app.get('/register', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/register.html'))
+  // res.sendFile(path.join(__dirname, '/views/register.html'))
+  res.sendFile(`${__dirname}/views/register.html`)
 });
 
 app.listen(port, () => {
